bad1d06c93fce54051ada5831eeae96cb40044a5
Gianluigi <50233545+RallyTuning@users.noreply.github.com>
2022-08-31 22:33:29 +0200

    funziona funziona

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace InitianPositionApp
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
+            this.Pnl_Centrale = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Btn_Chiudi = new System.Windows.Forms.ToolStripButton();
+            this.Btn_Riavvia = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.Txt_Restart = new System.Windows.Forms.ToolStripTextBox();
+            this.Lbl_Timer = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pnl_Centrale
+            // 
+            this.Pnl_Centrale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pnl_Centrale.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Centrale.Name = "Pnl_Centrale";
+            this.Pnl_Centrale.Size = new System.Drawing.Size(428, 234);
+            this.Pnl_Centrale.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Btn_Chiudi,
+            this.Btn_Riavvia,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.Txt_Restart,
+            this.Lbl_Timer,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 234);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(428, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Btn_Chiudi
+            // 
+            this.Btn_Chiudi.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Chiudi.Image")));
+            this.Btn_Chiudi.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Btn_Chiudi.Name = "Btn_Chiudi";
+            this.Btn_Chiudi.Size = new System.Drawing.Size(62, 22);
+            this.Btn_Chiudi.Text = "&Chiudi";
+            this.Btn_Chiudi.Click += new System.EventHandler(this.Btn_Chiudi_Click);
+            // 
+            // Btn_Riavvia
+            // 
+            this.Btn_Riavvia.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Riavvia.Image")));
+            this.Btn_Riavvia.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Btn_Riavvia.Name = "Btn_Riavvia";
+            this.Btn_Riavvia.Size = new System.Drawing.Size(64, 22);
+            this.Btn_Riavvia.Text = "&Riavvia";
+            this.Btn_Riavvia.Click += new System.EventHandler(this.Btn_Riavvia_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(46, 22);
+            this.toolStripLabel1.Text = "Restart:";
+            // 
+            // Txt_Restart
+            // 
+            this.Txt_Restart.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Txt_Restart.Name = "Txt_Restart";
+            this.Txt_Restart.Size = new System.Drawing.Size(100, 25);
+            this.Txt_Restart.Text = "60";
+            this.Txt_Restart.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_Restart_KeyPress);
+            this.Txt_Restart.TextChanged += new System.EventHandler(this.Txt_Restart_TextChanged);
+            // 
+            // Lbl_Timer
+            // 
+            this.Lbl_Timer.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.Lbl_Timer.Name = "Lbl_Timer";
+            this.Lbl_Timer.Size = new System.Drawing.Size(49, 22);
+            this.Lbl_Timer.Text = "00:00:00";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(28, 22);
+            this.toolStripLabel2.Text = "min";
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(428, 259);
+            this.Controls.Add(this.Pnl_Centrale);
+            this.Controls.Add(this.toolStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Shown += new System.EventHandler(this.Frm_Main_Shown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_Centrale;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Btn_Chiudi;
+        private System.Windows.Forms.ToolStripButton Btn_Riavvia;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox Txt_Restart;
+        private System.Windows.Forms.ToolStripLabel Lbl_Timer;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InitianPositionApp
+{
+    public partial class Frm_Main : Form
+    {
+
+        #region Variabili
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int cx, int cy, bool repaint);
+
+        readonly string appPath = "C:\\Users\\rally\\AppData\\Local\\Programs\\Reolink\\Reolink.exe";
+        readonly int sinistra = 380;
+        readonly int barra = 270;
+        int restartTimerMin = 1;
+
+        readonly DateTime adesso = DateTime.Now;
+
+        ProcessStartInfo psi;
+        Process p;
+
+        #endregion
+
+        #region Handles
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+
+            this.Location = Screen.AllScreens[0].WorkingArea.Location;
+            Size s = Screen.AllScreens[0].WorkingArea.Size;
+            this.Size = new Size(s.Width - sinistra, s.Height);
+
+            this.Focus();
+            toolStrip1.Focus();
+
+            //Per sicurezza
+            if (File.Exists("stop"))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Frm_Main_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                ReStarter();
+
+                psi = new ProcessStartInfo(appPath)
+                {
+                    CreateNoWindow = true,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = false,
+                    RedirectStandardError = false
+                };
+
+                p = Process.Start(psi);
+                p.WaitForInputIdle();
+
+                Thread.Sleep(500);
+
+                SetParent(p.MainWindowHandle, Pnl_Centrale.Handle);
+
+                psi.WindowStyle = ProcessWindowStyle.Maximized;
+
+                MoveWindow(p.MainWindowHandle, -barra, 0, this.Width + barra, this.Height - toolStrip1.Height, true);
+
+                //CenterToScreen();
+                psi.WindowStyle = ProcessWindowStyle.Maximized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Btn_Chiudi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                p.Close();
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Btn_Riavvia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                p.Close();
+                Application.Restart();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Txt_Restart_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar); //&& !char.IsControl(e.KeyChar);
+        }
+
+        private void Txt_Restart_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Txt_Restart.Text)) { return; }
+                if (Int32.Parse(Txt_Restart.Text) <= 0) { return; }
+
+                restartTimerMin = Int32.Parse(Txt_Restart.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        #endregion
+
+        #region void
+
+        private async void ReStarter()
+        {
+            await Task.Run(() =>
+            {
+                DateTime dopo = adesso.AddMinutes(restartTimerMin);
+                TimeSpan t = adesso.AddMinutes(restartTimerMin) - DateTime.Now;
+
+                while (t.Ticks >= 0)
+                {
+                    t = adesso.AddMinutes(restartTimerMin) - DateTime.Now;
+
+                    this.Invoke((System.Action)(() =>
+                    {
+                        Lbl_Timer.Text = t.ToString(@"hh\:mm\:ss");
+                    }));
+
+
+                    Thread.Sleep(500);
+                }
+            });
+
+            p.Close();
+            Application.Restart();
+        }
+
+        #endregion
+
+    }
+}
